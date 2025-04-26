5a18e0c5675b5d2cd9e46f230d923e14aebbab2f
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:03:47 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -1,54 +1,66 @@
 provider "azurerm" {
-  tenant_id       = "${var.tenant_id}"
-  subscription_id = "${var.subscription_id}"
-  client_id       = "${var.client_id}"
-  client_secret   = "${var.client_secret}"
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
   features {}
 }
 terraform {
   backend "azurerm" {
-    storage_account_name = ""
-    container_name       = ""
-    key                  = ""
-    access_key           = ""
+    storage_account_name = "tfstate1258227141"
+    container_name       = "tfstate"
+    key                  = "*projectEnsuringQualityReleases.tf"
+    access_key           = "tVCGyJBXlHaHnjhM7G1OQDXNpbyNoiHHiPVAeRZQ5vZFRSYNbHKNva3cl6bGyFBfH58vwhFh0VSz+AStvj0haQ=="
   }
 }
 module "resource_group" {
-  source               = "../../modules/resource_group"
-  resource_group       = "${var.resource_group}"
-  location             = "${var.location}"
+  source         = "../../modules/resource_group"
+  resource_group = var.resource_group_name
+  #location             = "${var.location}"
 }
 module "network" {
   source               = "../../modules/network"
-  address_space        = "${var.address_space}"
-  location             = "${var.location}"
-  virtual_network_name = "${var.virtual_network_name}"
-  application_type     = "${var.application_type}"
+  address_space        = var.address_space
+  location             = var.location
+  virtual_network_name = var.virtual_network_name
+  application_type     = var.application_type
   resource_type        = "NET"
-  resource_group       = "${module.resource_group.resource_group_name}"
-  address_prefix_test  = "${var.address_prefix_test}"
+  resource_group       = module.resource_group.resource_group_name
+  address_prefix_test  = var.address_prefix_test
 }
 
 module "nsg-test" {
-  source           = "../../modules/networksecuritygroup"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
-  resource_type    = "NSG"
-  resource_group   = "${module.resource_group.resource_group_name}"
-  subnet_id        = "${module.network.subnet_id_test}"
-  address_prefix_test = "${var.address_prefix_test}"
+  source              = "../../modules/networksecuritygroup"
+  location            = var.location
+  application_type    = var.application_type
+  resource_type       = "NSG"
+  resource_group      = module.resource_group.resource_group_name
+  subnet_id           = module.network.subnet_id_test
+  address_prefix_test = var.address_prefix_test
 }
 module "appservice" {
   source           = "../../modules/appservice"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
+  location         = var.location
+  application_type = var.application_type
   resource_type    = "AppService"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = module.resource_group.resource_group_name
 }
 module "publicip" {
   source           = "../../modules/publicip"
-  location         = "${var.location}"
-  application_type = "${var.application_type}"
+  location         = var.location
+  application_type = var.application_type
   resource_type    = "publicip"
-  resource_group   = "${module.resource_group.resource_group_name}"
+  resource_group   = module.resource_group.resource_group_name
+}
+
+module "virtual_machine" {
+  source               = "../../modules/vm"
+  nic_name             = var.nic_name
+  location             = var.location
+  resource_group       = module.resource_group.resource_group_name
+  subnet_id            = module.network.subnet_id_test
+  public_ip_address_id = module.publicip.public_ip_address_id
+  vm_name              = var.vm_name
+  admin_username       = "azureuseradmin"
+  username             = "azureuseradmin"
 }
