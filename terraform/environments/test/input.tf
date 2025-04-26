25953d0e1db7097e2d80b9a07bea6162cf890a85
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:02:53 +0200

    Update input.tf

--- terraform/environments/test/input.tf
+++ terraform/environments/test/input.tf
@@ -5,12 +5,36 @@ variable "client_secret" {}
 variable "tenant_id" {}
 
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
-variable "application_type" {}
+variable "location" {
+  description = "The Azure Region where the Resource Group should exist."
+  type        = string
+}
+variable "resource_group_name" {
+  description = "he name of the resource group in which to create the subnet. This must be the resource group that the virtual network resides in"
+  type        = string
+}
+variable "application_type" {
+  type = string
+}
 
 # Network
-variable virtual_network_name {}
-variable address_prefix_test {}
-variable address_space {}
+variable "virtual_network_name" {
+  description = "The name of the virtual network to which to attach the subnet. Changing this forces a new resource to be created"
+  type        = string
+}
+variable "address_prefix_test" {
+  description = "The address prefixes to use for the subnet."
+  type        = string
+}
+variable "address_space" {
+  description = "The address space that is used the virtual network. You can supply more than one address space."
+  type        = string
+}
 
+#vm variables
+variable "nic_name" {
+  type = string
+}
+variable "vm_name" {
+  type = string
+}
