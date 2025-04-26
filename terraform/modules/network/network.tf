ce76eb9778571cd8307f1e15bd8a8dae010c667b
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:40:38 +0200

    Update network.tf

--- terraform/modules/network/network.tf
+++ terraform/modules/network/network.tf
@@ -1,6 +1,6 @@
 resource "azurerm_virtual_network" "test" {
   name                 = "${var.application_type}-${var.resource_type}"
-  address_space        = "${var.address_space}"
+  address_space        = ["${var.address_space}"]
   location             = "${var.location}"
   resource_group_name  = "${var.resource_group}"
 }
@@ -8,5 +8,5 @@ resource "azurerm_subnet" "test" {
   name                 = "${var.application_type}-${var.resource_type}-sub"
   resource_group_name  = "${var.resource_group}"
   virtual_network_name = "${azurerm_virtual_network.test.name}"
-  address_prefix       = "${var.address_prefix_test}"
+  address_prefixes      = ["${var.address_prefix_test}"]
 }
