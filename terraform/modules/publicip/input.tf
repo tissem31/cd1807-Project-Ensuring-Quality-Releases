bfbbaf8900d7ffb4c0c99a015af3025fb49f2f08
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:06:23 +0200

    Update input.tf

--- terraform/modules/publicip/input.tf
+++ terraform/modules/publicip/input.tf
@@ -1,6 +1,16 @@
 # Resource Group/Location
-variable "location" {}
-variable "resource_group" {}
+variable "location" {
+  description = "The Azure Region where the Resource Group should exist."
+  type        = string
+}
+variable "resource_group" {
+  description = "the name of the resource group in which to create the subnet. This must be the resource group that the virtual network resides in"
+  type        = string
+}
 
-variable "application_type" {}
-variable "resource_type" {}
+variable "application_type" {
+  type = string
+}
+variable "resource_type" {
+  type = string
+}
