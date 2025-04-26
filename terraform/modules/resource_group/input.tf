a65942e63bd72fe58987a1ca71fa91068208c272
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:07:13 +0200

    Update input.tf

--- terraform/modules/resource_group/input.tf
+++ terraform/modules/resource_group/input.tf
@@ -1,3 +1,9 @@
 # Resource Group
-variable location {}
-variable "resource_group" {}
+# variable location {
+#     description = "The Azure Region where the Resource Group should exist."
+#     type = string
+# }
+variable "resource_group" {
+    description = "the name of the resource group in which to create the subnet. This must be the resource group that the virtual network resides in"
+    type = string
+}
