522d1c6dd30a15aa1134b45d62ee3fb4f165e964
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-04-26 15:07:56 +0200

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,30 @@
 
+
+variable "nic_name" {
+  
+}
+
+variable "location" {
+  
+}
+variable "resource_group" {
+  
+}
+variable "subnet_id" {
+  
+}
+variable "vm_name" {
+  
+}
+variable "admin_username" {
+
+}
+# variable "network_interface_ids" {
+  
+# }
+variable "username" {
+
+}
+variable "public_ip_address_id" {
+  
+}
