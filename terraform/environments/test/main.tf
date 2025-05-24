3efca1b7247f30765369d569649b2997732a2a60
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-05-24 23:34:00 +0200

    Update main.tf

--- terraform/environments/test/main.tf
+++ terraform/environments/test/main.tf
@@ -6,14 +6,14 @@ provider "azurerm" {
   features {}
 }
 
-terraform {
-  backend "azurerm" {
-    storage_account_name = "tfstate2797731198"
-    container_name       = "tfstate"
-    key                  = "projectEnsuringQualityReleases.tfstate"
-    access_key           = "C2HtFMMewOzFhNgaisF7Fo6BTBt/YgqNkj7iyIkSt5zM2rnaYqgNvf+fHEDtIqgyZavsRJbCOJjn+AStOVfC2g=="
-  }
-}
+# terraform {
+#   backend "azurerm" {
+#     storage_account_name = "tfstate2797731198"
+#     container_name       = "tfstate"
+#     key                  = "projectEnsuringQualityReleases.tfstate"
+#     access_key           = "C2HtFMMewOzFhNgaisF7Fo6BTBt/YgqNkj7iyIkSt5zM2rnaYqgNvf+fHEDtIqgyZavsRJbCOJjn+AStOVfC2g=="
+#   }
+# }
 
 module "resource_group" {
   source         = "../../modules/resource_group"
