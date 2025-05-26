fdd34bd45344bfeaabfdbd566577fabfecf739ce
Ibtissem SEGHIER <57727945+tissem31@users.noreply.github.com>
2025-05-27 00:28:54 +0200

    Update backend.tf

--- terraform/environments/test/backend.tf
+++ terraform/environments/test/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    storage_account_name = "tfstate20128356"
+    storage_account_name = "tfstate5034440"
     container_name       = "tfstate"
     key                  = "projectEnsuringQualityReleases.tfstate"
-    access_key           = "pPmx+OYk2P4tx8VDcYsX94DO23QHV3DiOVIDtSpTbtdI2pJkpmt/wFCCSBr0FmB306QHxtOo+lLW+AStL0Y0BQ=="
+    access_key           = "cCy3ixBNuDq9HRTQGzTZ1gTka2hqWAAlhC+VIA4AwE37fap2k0iKm+izpSuFuu76VkvgvJ3t3ze++AStmFMdHA=="
   }
 }
